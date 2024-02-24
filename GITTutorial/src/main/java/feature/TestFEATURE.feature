20b30a0f044332dd116461d7dd479b42425dff4c
anand <anand@Anandh>
2024-02-24 20:19:55 +0530

    Commit from eclipse

--- GITTutorial/src/main/java/feature/TestFEATURE.feature
+++ GITTutorial/src/main/java/feature/TestFEATURE.feature
@@ -2,3 +2,6 @@ Feature: To Test the GIt functionality
 @TestOne
 Scenario: To verify GIT fetch functionality
 Given commit from first project
+@TestTwo
+Scenario: To verify GIT fetch functionality
+Given commit from first project
--- GITTutorial/src/main/java/stepdef/TestStepDefs.java
+++ GITTutorial/src/main/java/stepdef/TestStepDefs.java
@@ -5,6 +5,12 @@ import io.cucumber.java.en.Given;
 public class TestStepDefs {
 	@Given("commit from first project")
 	public void commit_from_first_project() {
+		System.out.println("Changed from local");
+
+	}
+	@Given("commit from second project")
+	public void commit_from_second_project() {
+		System.out.println("Changed from local two");
 
 	}
 }
