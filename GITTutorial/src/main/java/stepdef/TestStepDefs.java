ba9d9052fb69886d5b02e2b9fccb882d97a41571
Anandh <76906302+Anandh-developer@users.noreply.github.com>
2024-02-24 20:18:05 +0530

    Update TestStepDefs.java

--- GITTutorial/src/main/java/stepdef/TestStepDefs.java
+++ GITTutorial/src/main/java/stepdef/TestStepDefs.java
@@ -3,8 +3,8 @@ package stepdef;
 import io.cucumber.java.en.Given;
 
 public class TestStepDefs {
-	@Given("commit from first project")
-	public void commit_from_first_project() {
+	@Given("commit from first project but git changed")
+	public void commit_from_first_project_but_git_changed() {
 
 	}
 }
